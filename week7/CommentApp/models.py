2008688d94927bc5fd4053cbf03b12c6fa0b1ede
tsitiii <ziontaa9@gmail.com>
2024-03-02 00:10:41 +0300

    delet

--- week7/CommentApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from BlogApp.models import Post
-from django.utils import timezone
-class Comment(models.Model):
-    Content=models.TextField(max_length=250)
-    Author=models.CharField(max_length=50)
-    PublishedDate=models.DateTimeField(default=timezone.now)
-    Post=models.ForeignKey(Post,  on_delete=models.CASCADE)
